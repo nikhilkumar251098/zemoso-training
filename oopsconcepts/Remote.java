82650f376a105478ab94a8e81db12103b0e5e5ef
Nikhil Kumar <nikhil.kumar@zemosolabs.com>
2022-01-04 14:26:31 +0530

    Adding OOPs application to git

---
+++ oopsconcepts/Remote.java
@@ -0,0 +1,17 @@
+package allinone.oopsconcepts;
+
+//abstraction
+public interface Remote {
+	void powerSwitch();
+	int decreaseVolume();
+	int increaseVolume();
+	double channelTuning(int channel);
+}
+
+/*abstract public class Remote{
+	abstract public boolean powerSwitch();
+	abstract public int decreaseVolume();
+	abstract public int increaseVloume();
+	abstract public double channelTuning(int channel);
+	
+}*/
